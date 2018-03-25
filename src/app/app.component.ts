fc98d617cb3a6af2c207bc708aa2ff8d41eaa473
Eman Herawy <emisk_vip@yahoo.com>
2018-03-25 02:49:52 +0200

    fix bugs in ng serve command

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { canBeNumber } from '../util/validation';
 import { TwitterService } from './twitter.service';
 
-const Web3 = require('web3');
-const contract = require('truffle-contract');
-// const metaincoinArtifacts = require('../../build/contracts/MetaCoin.json');
-const IPFS = require('ipfs')
-const OrbitDB = require('orbit-db')
-const ipfsOptions = {
-  EXPERIMENTAL: {
-    pubsub: true
-  },
-}
-
-// Create IPFS instance
-const ipfs = new IPFS(ipfsOptions)
 
 @Component({
   selector: 'app-root',
